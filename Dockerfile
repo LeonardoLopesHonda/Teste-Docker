97e0dd3388b525ea26b8a552d06488557f2e6d9f
Leonardo Lopes Honda <leozinhohondams@gmail.com>
2023-10-17 17:20:17 -0400

    feat: :technologist: Added automatic container update
    Added automatic container update using docker composer

--- Dockerfile
+++ Dockerfile
@@ -1,18 +1,11 @@
-FROM node:18-alpine3.15
+FROM node:18-alpine
 
-WORKDIR /app
+WORKDIR /usr/src/app 
 
 COPY package*.json ./
 
 RUN npm i
-# RUN npm i -g serve
-# RUN npm run build
-# RUN rm -fr node_modules
 
-COPY ./ ./
-
-ENV PORT=8081
-
-EXPOSE 8081
+COPY . .
 
 CMD [ "npm", "start" ]
--- app/app.js
+++ app/app.js
@@ -2,13 +2,11 @@
 
 const app = require("express")();
 const port = process.env.PORT || 8081;
-// const dockerPort = 3000;
 
 app.get("/", (req, res) => {
-  res.send("Docker is working fine");
+  res.send("Docker is working!");
 });
 
 app.listen(port, () => {
   console.log(`Servidor Rodando na url: http://localhost:${port}`);
-  //   console.log(`Container Rodando na url: http://localhost:${dockerPort}`);
 });
